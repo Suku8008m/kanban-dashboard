4a6d066fe72e09dd81786cd3fc5ce7b1375be9ef
K.Sukumar <146643923+Suku8008m@users.noreply.github.com>
2026-02-11 00:01:35 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AppProvider } from "./Context.jsx";
 import App from "./App.jsx";
-
+import { ToastContainer } from "react-toastify";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppProvider>
       <App />
     </AppProvider>
+    <ToastContainer />
   </StrictMode>,
 );
+
